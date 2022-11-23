5431c5695e04637f437f062607bf31ac16e9c533
samreddymuralinath <118870455+samreddymuralinath@users.noreply.github.com>
2022-11-23 09:51:55 +0530

    Update streamlit_app.py
    Replaced " with ' MOM'S

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Mom's new healthy diner')
+streamlit.title('My Mom"s new healthy diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry OatMeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
