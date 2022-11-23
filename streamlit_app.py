e7a2663fa54f1c0a6958dc9fb4c30fc660351a52
samreddymuralinath <118870455+samreddymuralinath@users.noreply.github.com>
2022-11-23 09:50:32 +0530

    Update streamlit_app.py
    added one more to breakfast menu and changed the Title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
-streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry OatMeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.title('My Mom's new healthy diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega 3 & Blueberry OatMeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
