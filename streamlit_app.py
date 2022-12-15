f1994626fe2fcc58bb74d4421f3b7b5734acd8ee
samreddymuralinath <118870455+samreddymuralinath@users.noreply.github.com>
2022-12-15 12:47:06 +0530

    Update streamlit_app.py
      #my_cnx.close()

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ except URLError as e:
 # write your own comment - what does this do?
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 
-streamlit.header("View Our Fruit List - Add Your Favorites!")
+streamlit.header('View Our Fruit List - Add Your Favorites!')
 
 #Snowflake-related functions
 def get_fruit_load_list():
@@ -59,7 +59,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
-    my_cnx.close()
+    #my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 
@@ -73,7 +73,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
-    my_cnx.close()
+    #my_cnx.close()
     streamlit.text(back_from_function)
 #streamlit.write('Thanks for adding ', add_my_fruit)
 
