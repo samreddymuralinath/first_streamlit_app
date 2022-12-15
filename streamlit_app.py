c48096dd4a92af72b77c326f4f2227f28c7d8d57
samreddymuralinath <118870455+samreddymuralinath@users.noreply.github.com>
2022-12-15 11:21:36 +0530

    Update streamlit_app.py
    # New Section to display Fruitvice API Response
    streamlit.header("Fruityvice Fruit Advice!")
    try: 
        fruit_choice = streamlit.text_input('What fruit would you like information about?')
        if not fruit_choice:
          streamlit.error('Please select a fruit to get information')
        else: 
        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
        streamlit.dataframe(fruityvice_normalized)
    
    except URLError as e: 
      streamlit.error()

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,19 +22,23 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+# New Section to display Fruitvice API Response
 streamlit.header("Fruityvice Fruit Advice!")
+try: 
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+      streamlit.error('Please select a fruit to get information')
+    else: 
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
+except URLError as e: 
+  streamlit.error()
 #streamlit.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+
 streamlit.stop()
 
 
